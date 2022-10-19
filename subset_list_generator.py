322f3ee03d7e6a3a67fd8bcb4a375acafb2e6e83
Satya Amarkant Marupureddy <amarkant23394@gmail.com>
2022-10-19 17:50:42 -0400

    Returns a list of unique subsets which satisfies the match of target number and the list is in form of pairs

---
+++ subset_list_generator.py
@@ -0,0 +1,14 @@
+ret_subset_sum_list = []
+def subset_sum(numbers, target, partial=[]):
+    s = sum(n for _, n in partial)
+
+    # check if the partial sum is equals to target
+    if s == target:
+        ret_subset_sum_list.append(partial)
+    if s >= target:
+        return  # if we reach the number why bother to continue
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        remaining = numbers[i + 1:]
+        subset_sum(remaining, target, partial + [n])
